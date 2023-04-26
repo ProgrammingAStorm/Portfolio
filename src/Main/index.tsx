3a65f60abd7140ceaa9d419e60005916e22138fa
Mark Pavel <markpavel02@gmail.com>
2023-04-26 13:02:06 -0400

    Removed Is Visible From Main

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,18 +1,11 @@
 import { ReactNode, useRef, useState } from "react"
 
-import "intersection-observer";
-
-import { useIsVisible } from 'react-is-visible'
-
 interface IMainProps {
     children: ReactNode[];
 }
 
 export default function Main({ children }: IMainProps) {
     const [currentChild, setCurrentChild] = useState(0)
-
-    const mainRef = useRef(null)
-    const isVisible = useIsVisible(mainRef);
 
     const handleLeft = () => {
         if(currentChild === children.length - 1) {
@@ -30,7 +23,7 @@ export default function Main({ children }: IMainProps) {
         }
     }
 
-    return <main ref={mainRef} id="main" className="main">
+    return <main id="main" className="main">
         <i className="main-arrow left" onClick={handleLeft}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
