5a1d89befc6b977334afc15a44cbaf87b4c919a2
Mark Pavel <markpavel02@gmail.com>
2023-04-26 13:20:22 -0400

    Header Arrow Doesn't Disappear

--- src/Hero/index.tsx
+++ src/Hero/index.tsx
@@ -22,7 +22,7 @@ const Hero: React.FC<IHeroProps> = ({ isClicked, setIsClicked }) => {
         <h2 className='sub-header'>Fullstack Web Developer</h2>
       </section>
 
-      <i className={`arrow ${isVisible ? 'visible' : ''} ${isClicked ? 'click' : ''}`} onClick={() => setIsClicked(true)}>
+      <i className={`arrow ${isVisible ? 'visible' : ''}`} onClick={() => setIsClicked(true)}>
         <a href="#main">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
