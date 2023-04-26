be86d037460ca277bd4e1c4c5cf733d943eac123
Mark Pavel <markpavel02@gmail.com>
2023-04-26 14:54:51 -0400

    Removed Leftover Code

--- src/Hero/index.tsx
+++ src/Hero/index.tsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
 interface IHeroProps {
-  isClicked: boolean;
   setIsClicked: Function;
 }
 
-const Hero: React.FC<IHeroProps> = ({ isClicked, setIsClicked }) => {
-  const [isVisible, setIsVisible] = useState(false);
-
+const Hero: React.FC<IHeroProps> = ({ setIsClicked }) => {
   return (
     <header className="header">
       <section className="header-content">
@@ -15,7 +12,7 @@ const Hero: React.FC<IHeroProps> = ({ isClicked, setIsClicked }) => {
         <h2 className='sub-header'>Fullstack Web Developer</h2>
       </section>
 
-      <i className={`arrow ${isVisible ? 'visible' : ''}`} onClick={() => setIsClicked(true)}>
+      <i className="arrow" onClick={() => setIsClicked(true)}>
         <a href="#main" onClick={() => setIsClicked(true)}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
