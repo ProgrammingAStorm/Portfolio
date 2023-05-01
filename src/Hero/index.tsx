d68bee950ee86614ed6fe1858c8ce296a28a037b
Mark Pavel <markpavel02@gmail.com>
2023-05-01 13:36:56 -0400

    Removed i Element To Fix Scrolling Behaviour

--- src/Hero/index.tsx
+++ src/Hero/index.tsx
@@ -13,16 +13,14 @@ const Hero: React.FC<IHeroProps> = ({ setIsClicked, setCurrentSection }) => {
         <h2 className='sub-header'>Fullstack Web Developer</h2>
       </section>
 
-      <i className="arrow" onClick={() => {
+      <a href="#main" className="arrow" onClick={() => {
         setCurrentSection(0);
         setIsClicked(true);
       }}>
-        <a href="#main">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
-          </svg>
-        </a>
-      </i>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+        </svg>
+      </a>
     </header>
   );
 };
