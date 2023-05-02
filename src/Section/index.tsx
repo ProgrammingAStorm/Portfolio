e2c1d6930c4a9154ab93471fd5e687bc1f0aa9d5
Mark Pavel <markpavel02@gmail.com>
2023-05-02 18:26:02 -0400

    Added Section Header Neon Shaddows

--- src/Section/index.tsx
+++ src/Section/index.tsx
@@ -9,21 +9,23 @@ interface ISectionProps {
 
 export default function Section({ header, subHeaders, animationName, children }: ISectionProps) {
     return <section className={`section ${animationName}`}>
-        <h2 className="section-header">
-            {header}
-        </h2>
+        <div className="section-headers">
+            <h2 className="section-header">
+                {header}
+            </h2>
 
-        <ul className="section-sub-headers">
-            {subHeaders.map((value, index) => {
-                return (
-                    <li key={index}>
-                        <h3>
-                            {value}
-                        </h3>
-                    </li>
-                );
-            })}
-        </ul>
+            <ul className="section-sub-headers">
+                {subHeaders.map((value, index) => {
+                    return (
+                        <li key={index}>
+                            <h3>
+                                {value}
+                            </h3>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
 
         <div className="section-content">
             <ul className="article-scroller">
