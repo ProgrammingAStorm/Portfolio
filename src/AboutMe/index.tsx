0f22da12836b88436d5aa731292591cf5f6bfe7f
Mark Pavel <markpavel02@gmail.com>
2023-05-09 13:14:00 -0400

    Adding dist

--- src/AboutMe/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function AboutMe() {
-    return <section className="section aboutme">
-        <h1 className="section-header">About Me</h1>
-    </section>
-}
--- src/ContactMe/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function ContactMe() {
-    return <section className="section contactme">
-        <h1 className="section-header">Contact Me</h1>
-    </section>
-}
