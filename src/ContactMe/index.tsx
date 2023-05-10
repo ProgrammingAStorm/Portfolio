8c18bb98a6a1eaea51369f3088c5efa0129c7880
Mark Pavel <markpavel02@gmail.com>
2023-05-10 17:53:11 -0400

    Contact Me Content And Styling

--- src/ContactMe/index.tsx
+++ src/ContactMe/index.tsx
@@ -1,5 +1,11 @@
 export default function ContactMe() {
     return <section className="section contactme">
-        <h1 className="section-header">Contact Me</h1>
+        <h2 className="section-header">Contact Me</h2>
+
+        <div>
+            <h3>Mark Pavel</h3>
+            <a href="mailto:markpavel02@gmail.com" className="link content">markpavel02@gmail.com</a>
+            <p className="content">(734) 341-9709</p>
+        </div>
     </section>
 }
