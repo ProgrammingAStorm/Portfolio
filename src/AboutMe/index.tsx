f32639cc58fab9784a72b31aa8ab0edcdf3be8eb
Mark Pavel <markpavel02@gmail.com>
2023-05-10 16:19:51 -0400

    Added About me Section + Section And Main Responsive Tweaks

--- src/AboutMe/index.tsx
+++ src/AboutMe/index.tsx
@@ -1,5 +1,13 @@
 export default function AboutMe() {
     return <section className="section aboutme">
         <h1 className="section-header">About Me</h1>
+
+        <p>Having recently graduated from a fullstack coding bootcamp, I am not only equiped with a thorough grasp of web development basics, but also the concepts essential to enterprise-level application development. As a developer, its my job to make you money by developing performant and scaling apps that maintain a high degree of SEO, as customer experience is directly related to conversions.</p>
+
+        <p>As someone who loves to try and learn new things, I have tried my hands at many languages and technologies. From MERN building simple MVC apps, to Flutter building multi-platform apps with Dart, to Android with Kotlin, to enterprise stacks like ASP.NET and AWS alongside SSR frameworks like NEXT 13, to even simple React apps with BaaS solutions like Firebase. I love breaking down problems, and I love to learn about and try out all the bleeding-edge technologies that are being pioneered to solve problems. As such, I am very good at learning, and always love looking for new things to learn about and try out.</p>
+
+        <p>The projects shown off here are only a fraction of the total projects I've worked on, so if you like what you see and would like to see more, I encourage you to checkout my <a href="https://github.com/ProgrammingAStorm" target="_blank" className="link">GitHub profile</a> for more of my work. </p>
+
+        <p className="self-center">Click <a href="/Portfolio/Mark-Pavel.pdf" target="_blank" className="link">here</a> for a copy of my resume.</p>
     </section>
 }
